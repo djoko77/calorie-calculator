4e20523ff771fef82d5637bf01e84b934f7e40ba
Rombie Lagunzad <41833766+djoko77@users.noreply.github.com>
2022-03-08 02:42:38 +0800

    add error handling

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
-// Variables
-var gender = document.querySelector('input[name="female-male"]:checked').value;
-var heightFeet = document.getElementById('height-feet').value;
-var heightInches = document.getElementById('height-inches').value;
-var weight = document.getElementById('weight').value;
-var age = document.getElementById('age').value;
-var activity = document.getElementById("activityLevel").value;
-var bmr = "";
-
 // Button
 var button = document.getElementById('calculate');
 
 // Event listener for the button
 button.addEventListener('click', () => {
+    
+    var gender = document.querySelector('input[name="female-male"]:checked').value;
+    var heightFeet = document.getElementById('height-feet').value;
+    var heightInches = document.getElementById('height-inches').value;
+    var weight = document.getElementById('weight').value;
+    var age = document.getElementById('age').value;
+    var activity = document.getElementById("activityLevel").value;
+
     console.log(gender);
     console.log(heightFeet);
     console.log(heightInches);
     console.log(weight);
     console.log(age);
     console.log(activity);
-    calculate();
+
+    if (heightFeet == "" || heightInches == "") {
+        document.getElementById('message').innerHTML = "Please enter your height in feet and inches"
+    }
+    else if (weight == "" ) {
+        document.getElementById('message').innerHTML = "Please enter your weight in lbs."
+    }
+    else if (age == "" ) {
+        document.getElementById('message').innerHTML = "Please enter your age in years"
+    }
+    else {
+    
+        document.getElementById('message').innerHTML = ""
+    calculate(gender, heightFeet, heightInches, weight, age, activity);
+    }
 });
 
-function calculate() {
+function calculate(gender, heightFeet, heightInches, weight, age, activity) {
 
     let kgs = (parseFloat(weight)*0.45359237);
     console.log(kgs)
@@ -32,11 +45,11 @@ function calculate() {
     console.log(gender);
 
     if (gender === "male") {
-        bmr = (10 * kgs) + (6.25 * cms) - (5*(parseInt(age))) + 5 ;
+        let bmr = (10 * kgs) + (6.25 * cms) - (5*(parseInt(age))) + 5 ;
         var finalBMR = bmr * (parseFloat(activity));
     }
     else if (gender === "female") {
-        bmr = (10 * kgs) + (6.25 * cms) - (5*(parseInt(age))) - 161 ;
+        let bmr = (10 * kgs) + (6.25 * cms) - (5*(parseInt(age))) - 161 ;
         var finalBMR = bmr * (parseFloat(activity));
     }
 
